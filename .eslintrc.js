2a02241a90527cbb775389cd0d128d5b58a75826
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2019-11-03 19:34:50 +0700

    feat: start new project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "react-app",
+  ],
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+  }
+};

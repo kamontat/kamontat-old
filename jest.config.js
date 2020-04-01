b3a77e74e669abc311f35ba061161196f8fa8617
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-04-01 20:17:31 +0700

    fix(test): update jest config file to support new project layout

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/assets.js",
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/assets.js",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
@@ -14,5 +14,5 @@ module.exports = {
     __PATH_PREFIX__: "",
   },
   testURL: "http://localhost",
-  setupFiles: ["<rootDir>/loadershim.js"],
+  setupFiles: ["<rootDir>/test/loadershim.js"],
 };

ff3cb660d2b58873dd68ea4f6650b45746763dbf
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-04-01 21:51:17 +0700

    impr(config): add new default value for react props

--- src/typescript/properties.ts
+++ src/typescript/properties.ts
@@ -1,3 +1,7 @@
-export interface DefaultProps {
-  children?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+import { ReactNodeLike } from "prop-types";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface DefaultProps<T = any> {
+  children?: ReactNodeLike;
+  pageContext?: T;
 }

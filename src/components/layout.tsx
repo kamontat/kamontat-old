b4543deb7e5546ce250bcd88e7735c09c6c07a16
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-04-03 16:41:04 +0700

    feat(components): support new cookie hook and data consent

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
+import { CookiesProvider } from "react-cookie";
+import CookieConsent from "react-cookie-consent";
 
 import layout from "../scss/bulma.module.scss";
 
-import { DefaultProps } from "../typescript/properties";
+import { DefaultProps } from "../typescript/src/models/properties";
 
 const Layout = (props: DefaultProps) => {
   return (
     <React.Fragment>
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet" />
-      </Helmet>
-      <div className={layout.container}>{props.children}</div>
+      <CookiesProvider>
+        <Helmet>
+          <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet" />
+        </Helmet>
+
+        <div className={layout.container}>{props.children}</div>
+
+        <CookieConsent
+          location="bottom"
+          buttonText="I understand"
+          style={{ background: "#000000" }}
+          cookieName="is-analytics-enabled"
+          enableDeclineButton
+        >
+          This website uses cookies to enhance the user experience.
+        </CookieConsent>
+      </CookiesProvider>
     </React.Fragment>
   );
 };

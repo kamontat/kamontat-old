9f1385131930a9249f1a4b7d66a75be42db60e56
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-04-03 16:42:24 +0700

    chore(test): add correctly unittest

---
+++ src/components/__test__/layout.spec.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Layout from "../layout";
+
+describe("Layout component", () => {
+  it("should set container children element to data", () => {
+    const wrapper = shallow(<Layout>data</Layout>);
+    expect(wrapper.find(".container")).toHaveLength(1);
+  });
+});
--- src/components/__test__/seo.spec.tsx
+++ src/components/__test__/seo.spec.tsx
@@ -1,36 +1,42 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { mount } from "enzyme";
 
 import { PureSEO } from "../seo";
+import Helmet from "react-helmet";
 
-describe("Header", () => {
+describe("Seo component", () => {
   const data = {
-    title: "",
-    description: "",
+    title: "First title",
+    description: "Description",
     lang: "en",
     meta: [],
     site: {
       siteMetadata: {
-        title: "",
-        description: "",
+        title: "Second title",
+        description: "Description",
         app: {
-          name: "",
-          version: "",
+          name: "hello",
+          version: "v1.0.0",
         },
-        buildTime: "",
+        buildTime: "123456",
       },
     },
     twitter: {
-      username: "",
+      username: "name",
     },
     github: {
-      username: "",
-      url: "",
+      username: "ghname",
+      url: "https://gh.com",
     },
   };
 
-  it("renders correctly", () => {
-    const tree = renderer.create(<PureSEO data={data} />).toJSON();
-    expect(tree).toMatchSnapshot();
+  it("render correctly", () => {
+    const component = mount(<PureSEO data={data} />);
+
+    const helmet = Helmet.peek();
+
+    expect(helmet).toMatchSnapshot("SEO component");
+
+    component.unmount();
   });
 });
